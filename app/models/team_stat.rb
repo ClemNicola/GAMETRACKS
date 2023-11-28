193ddeccf2ecb03f72fe135907a2be5d10e2d2b8
Alexis Dutoya <alexis.dutoya@gmail.com>
2023-11-28 10:48:42 +0100

    refacto validates done

--- app/models/team_stat.rb
+++ app/models/team_stat.rb
@@ -2,21 +2,21 @@ class TeamStat < ApplicationRecord
   belongs_to :team
   has_many :game_stats, through: :teams
 
-  validates :total_wins, numericality: { only_integer: true }
-  validates :total_losses, numericality: { only_integer: true }
-  validates :total_point, numericality: { only_integer: true }
-  validates :total_fg_made, numericality: { only_integer: true }
-  validates :total_fg_attempt, numericality: { only_integer: true }
-  validates :total_threep_made, numericality: { only_integer: true }
-  validates :total_threep_attempt, numericality: { only_integer: true }
-  validates :total_ft_made, numericality: { only_integer: true }
-  validates :total_ft_attempt, numericality: { only_integer: true }
-  validates :total_off_rebound, numericality: { only_integer: true }
-  validates :total_def_rebound, numericality: { only_integer: true }
-  validates :total_assist, numericality: { only_integer: true }
-  validates :total_turnover, numericality: { only_integer: true }
-  validates :total_steal, numericality: { only_integer: true }
-  validates :total_block, numericality: { only_integer: true }
-  validates :total_fault, numericality: { only_integer: true }
-  validates :total_evaluation, numericality: { only_integer: true }
+  validates :total_wins,
+            :total_losses,
+            :total_point,
+            :total_fg_made,
+            :total_fg_attempt,
+            :total_threep_made,
+            :total_threep_attempt,
+            :total_ft_made,
+            :total_ft_attempt,
+            :total_off_rebound,
+            :total_def_rebound,
+            :total_assist,
+            :total_turnover,
+            :total_steal,
+            :total_block,
+            :total_fault,
+            :total_evaluation, numericality: { only_integer: true }
 end
