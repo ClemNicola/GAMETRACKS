80de81b6db045f6468b150433cd16ccb01e0649b
Clement Nicolas <clementnicolasprobis@gmail.com>
2023-11-29 14:39:39 +0100

    creation controller

---
+++ app/controllers/game_stats_controller.rb
@@ -0,0 +1,6 @@
+class GameStatsController < ApplicationController
+
+  def show
+    
+  end
+end
---
+++ app/controllers/participations_controller.rb
@@ -0,0 +1,8 @@
+class ParticipationsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end 
+end
