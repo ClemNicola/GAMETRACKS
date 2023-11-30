2c34362c31ab52dd5ef88a515f5eabec06cd3ee8
Alexis Dutoya <alexis.dutoya@gmail.com>
2023-11-30 17:38:05 +0100

    view

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -4,8 +4,23 @@ class ParticipationsController < ApplicationController
     @user = current_user
     @team = Team.where(coach: @user)
     @players_list = @team.first.players.all
+    raise
+    @game = Game.find(params[:game_id])
   end
 
   def create
+    @participation = Participation.new(participation_params)
+    if @participation.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def participation_params
+    params.require(:participation).permit(:team_id, :game_id, :player_id)
+  end
+
 end
