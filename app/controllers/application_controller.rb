e665a5bd6538bd570c80226ae4b0488d1a5fe873
abdeslam sefrioui <abdeslamse@hotmail.com>
2023-12-01 15:47:08 +0100

    redirect ok and quick fix for city

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :category, :date_of_birth])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :category, :date_of_birth])
+  end
+
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
