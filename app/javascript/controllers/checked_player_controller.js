841c47cdf330c91d3bee97e82f812576b3048fee
Alexis Dutoya <alexis.dutoya@gmail.com>
2023-12-05 15:35:09 +0100

    js oki

--- app/javascript/controllers/checked_player_controller.js
+++ app/javascript/controllers/checked_player_controller.js
@@ -4,15 +4,23 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [
     'button',
-    'checkboxesContainer'
+    'checkboxesContainer',
+    'playersContainer',
+    'buttonNext',
+    'count'
   ]
 
   connect() {
+    console.log('yo');
   }
 
   select(event) {
+    event.preventDefault()
     const playerImg = event.currentTarget.querySelector('.imgimgimg')
-    playerImg.classList.add('selected')
+    playerImg.classList.toggle('selected')
+    const checkbox = event.currentTarget.querySelector('input[type=checkbox]')
+    checkbox.checked = !checkbox.checked
+    this.countPlayers()
   }
 
   countPlayers() {
@@ -22,5 +30,24 @@ export default class extends Controller {
     } else {
       this.buttonTarget.hidden = true
     }
+  }
+
+  selectCard(event) {
+    event.preventDefault()
+    const card = event.currentTarget
+    const checkbox = card.querySelector('input[type=checkbox]')
+    const icon = card.querySelector('.fa-regular')
+    icon.hidden = !icon.hidden
+    checkbox.checked = !checkbox.checked
+    card.classList.toggle('checked')
+    this.countPlayersCard()
+
+  }
+
+
+  countPlayersCard() {
+    const checkedCheckboxes = this.playersContainerTarget.querySelectorAll('input[type=checkbox]:checked').length
+    this.buttonNextTarget.hidden = checkedCheckboxes < 5;
+    this.countTarget.innerText = `${checkedCheckboxes}/10`
   }
 }
